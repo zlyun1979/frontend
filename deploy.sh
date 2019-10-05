1f725751113e8415c8b0d6e706f43819ab234eb5
zlyun1979 <55897807+zlyun1979@users.noreply.github.com>
2019-10-05 00:04:42 -0700

    fix deploy.sh file typo

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 kubectl apply -f ./frontend-deployment.yaml
 kubectl apply -f ./frontend-ip-service.yaml
-kubectl set image deployments/frontend frontend=zlyun1979/frontend:$SHA
+kubectl set image deployments/frontend-deployment frontend=zlyun1979/frontend:$SHA
