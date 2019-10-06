cd25522a461b2f92257707dd98178a4dfb6bd1c6
zlyun1979 <55897807+zlyun1979@users.noreply.github.com>
2019-10-06 16:13:25 -0700

    fix ingress-service

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 kubectl apply -f ./frontend-deployment.yaml
 kubectl apply -f ./frontend-ip-service.yaml
+kubectl apply -f ./ingress-service.yaml
 kubectl set image deployments/frontend-deployment frontend=zlyun1979/frontend:$SHA
